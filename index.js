c6b962539f33c4f7fe4fb1ea84fb34630292d7ab
Prasanth Vaaheeswaran <prasanthv@icloud.com>
2018-05-22 23:41:18 -0400

    Working on args

---
+++ index.js
@@ -0,0 +1,26 @@
+const promisify = require('util').promisify;
+const cp = require('child_process');
+const args = require('args');
+const execPromise = promisify(cp.exec);
+
+args
+  .option('remoteName', 'Remote ref name')
+  .option('remoteUrl', 'The git url of remote destination')
+  .option('ignore-tags', `Don't push tags`)
+  .option('dry-run', 'Dry run only');
+
+const flags = args.parse(process.argv);
+const dryRun = flags.dryRun;
+
+// @todo validate remote protocol is supported
+if (!flags.remoteUrl) {
+  console.log('Remote required.');
+  args.showHelp();
+}
+
+console.log(flags);
+
+async function run(options) {
+  const command = `git push --dry-run ${options.remoteUrl}`;
+}
+
